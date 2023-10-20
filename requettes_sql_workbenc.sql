be44d3b2d3f233bf8ef815b603c12ceafbb0ac5c
marti <marti.wills@MacBook-Air-de-marti.local>
2023-10-20 02:19:06 +0200

    suite

--- requettes_sql_workbenc.sql
+++ requettes_sql_workbenc.sql
@@ -19,7 +19,7 @@ WHERE a.idArtiste = r.idActeur
 AND a.nom="Willis" 
 AND a.prénom="Bruce";
 
--- Films artistes roles notes
+-a
 
 -- Exo 4 Qui est le réalisateur de Memento. 
 SELECT artiste.nom,artiste.prénom  
@@ -66,40 +66,100 @@ AND internaute.prénom="Prénom0";
 
 -- Exo 10 Films dont le réalisateur est Tim Burton, et l’un des acteurs
 -- Johnny Depp. 
-SELECT titre 
-FROM film,artiste
-WHERE film.idRéalisateur=artiste.idArtiste
-AND artiste.nom ="Depp"
-AND artiste.prénom ="Johnny"
-
-AND artiste.nom ="Tim"
-AND artiste.prénom ="Burton";
-pas encore finit la requette 10
+select titre from role,artiste,film
+WHERE role.idActeur = artiste.idArtiste
+AND artiste.nom="Depp"
+AND artiste.prénom="johnny"
+AND film.idFilm = role.idFilm
+AND titre IN(
+select titre from film,artiste
+WHERE film.idRéalisateur = artiste.idArtiste
+AND artiste.nom="Burton"
+AND artiste.prénom="Tim"
+);
 
 -- Exo 11 Titre des films dans lesquels a joué ́Woody Allen. Donner aussi
 -- le rôle.
 
+SELECT titre,role.nomRôle FROM film,artiste,role
+WHERE  artiste.idArtiste=role.idActeur
+AND film.idFilm=role.idFilm
+AND artiste.nom="allen"
+AND artiste.prénom="Woody";
+
 -- Exo 12 Quel metteur en scène a tourné dans ses propres films ? Donner
 -- le nom, le rôle et le titre des films.
-
-
-
+SELECT titre, artiste.nom,artiste.prénom,role.nomRôle FROM film,artiste,role
+WHERE film.idFilm=role.idFilm
+AND film.idRéalisateur=role.idActeur
+AND film.idRéalisateur=artiste.idArtiste;
 
 -- Exo 13 Titre des films de Quentin Tarantino dans lesquels il n’a pas
 -- joué
+
+
+
 -- Exo 14 Quel metteur en scène a tourné ́en tant qu’acteur ? Donner le
 -- nom, le rôle et le titre des films dans lesquels cet artiste a joué.
+
+select  nomRôle ,artiste.nom,artiste.prénom,titre 
+from film,artiste,role
+ where film.idRéalisateur = artiste.idArtiste
+ AND artiste.idArtiste=role.idActeur
+ AND  artiste.idArtiste IN (
+SELECT artiste.idArtiste from artiste,film,role
+WHERE artiste.idArtiste = role.idActeur
+AND role.idFilm = film.idFilm
+ );
+
 -- Exo 15 Donnez les films de Hitchcock sans James Stewart
 -- Exo 16 Dans quels films le réalisateur a-t-il le même prénom que l’un
 -- des interprètes ? (titre, nom du réalisateur, nom de l’interprète). Le
 -- réalisateur et l’interprète ne doivent pas être la même personne.
+
 -- Exo 17 Les films sans rôle
+select titre from film,role
+where role.idFilm=film.idFilm
+AND role.idActeur=NULL;
+
+
 -- Exo 18 Quelles sont les films non notés par l'internaute Prénom1 Nom1
+SELECT titre  FROM film,notation,internaute
+WHERE notation.idFilm=film.idFilm
+AND internaute.email=notation.email
+AND internaut.prénom="Prénom1"
+AND internaut.nom="Nom1"
+AND film.idFilm NOT IN (select idFilm from film);
+
+
 -- Exo 19 Quels acteurs n’ont jamais réalisé de film ?
+SELECT artiste.idArtiste, nom,prénom FROM artiste 
+WHERE artiste.idArtiste NOT IN(SELECT role.idActeur FROM role)  ;
+
+
 -- Exo 20 Quelle est la moyenne des notes de Memento
+SELECT  avg(note) FROM notation,film
+WHERE notation.idFilm=film.idFilm
+AND film.titre="Momento"
+GROUP BY film.titre;
+
 -- Exo 21 id, nom et prénom des réalisateurs, et nombre de films qu’ils
 -- ont tournés.
+SELECT  COUNT(*) as "nombres de films" ,artiste.idArtiste ,artiste.nom,artiste.prénom 
+FROM film,artiste
+WHERE artiste.idArtiste=film.idRéalisateur
+GROUP BY artiste.idArtiste,artiste.nom,artiste.prénom ;
+
 -- Exo 22 Nom et prénom des réalisateurs qui ont tourné au moins deux
 -- films.
--- Exo 23 Quels films ont une moyenne des notes supérieure à 7
+SELECT  artiste.nom,artiste.prénom  FROM film,artiste
+WHERE film.idRéalisateur=artiste.idArtiste
+GROUP BY artiste.idArtiste,artiste.nom,artiste.prénom
+HAVING COUNT(*) >=2;
 
+-- Exo 23 Quels films ont une moyenne des notes supérieure à 7
+SELECT  titre
+FROM film,notation
+WHERE film.idFilm = notation.idFilm
+GROUP BY titre
+HAVING AVG(note) > 7;
